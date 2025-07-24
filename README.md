8503ba72f7a5b080b7f30574ec8cd4db91b1c86f
JoBer <jonhebm@gmail.com>
2025-07-24 05:29:59 -0400

    logica mejorada

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Project for practice javascript.
 
 ## Screenshots
 
-![App screenshot](/%20Calculator%20-.png)
+![App screenshot](/%20Calculator%20-.png)```
--- app.js
+++ app.js
@@ -1,40 +1,91 @@
-//seleccionar la clase
-// luego crear la funcion
-//crear la clase y colocarla con toogle
-// crear el toogle
-
+//DOM
+// darck mode button
 const dMode = document.querySelector(".btn");
-const  divBox = document.querySelector(".container")
-
-const keys = document.querySelectorAll(".key");
+//container
+const divBox = document.querySelector('.container');
+const containerButtons= document.querySelector(".keys");
 const display__input = document.querySelector(".display .input");
 const display__output = document.querySelector(".display .output");
-let input = "";
 
-for (let key of keys) {
-  const value = key.dataset.key;
-  key.addEventListener("click", () => {
-    if (value == "clear") {
-      input = "";
-      display__input.innerHTML = " ";
-      display__output.innerHTML = " ";
-    } else if (value == "backS") {
-      input = input.slice(0, -1);
-      display__output.innerHTML = input;
-      const display__input = document.querySelector(".display . input");
-    } else if (value == "=") {
-      let result = eval(input);
-      display__output.innerHTML = result;
-    } else {
-      input += value;
-      display__input.innerHTML = input;
-    }
-  });
+//operator buttons
+const buttons = [ "<"  ,"AC" ,"0", "+", "9", "8", "7", "-", "5", "6", "4", "/", "3" ," 2" , " 1" , "%" ,"="]
+//calculator State
+let outputCalculo = "";
+
+
+// start logic for buttons
+buttons.forEach( text => {
+  const btn = document.createElement("button");
+  btn.textContent = text;
+  if(text=="="){
+    btn.classList.add("operadors");
+    btn.classList.add("equal");
+  }
+  //logic for operadors
+if(text == "+" || btn.textContent =="-" || btn.textContent == "%" || btn.textContent == "/" || btn.textContent == "<" || btn.textContent == "AC"){
+  btn.classList.add("operadors")
+  
 }
 
-   dMode.addEventListener("click",
-   function(){
-     divBox.classList.toggle("mDark")
-   }
+//intruducing text in input for every cliclk
+  btn.addEventListener("click", () => pushButtonCalc(text))
+  btn.classList.add("key");
+  containerButtons.appendChild(btn);
+});
+// end logic buttons
 
-   )
+
+//start logic calculator
+function  pushButtonCalc(text){
+    if(text === "AC"){
+      display__input.innerHTML = "";
+      display__output.innerHTML = "";
+      outputCalculo = "";
+  return;
+    }
+
+  if(text ==="<"){
+   let back = display__input.textContent.slice(0, -1)
+   display__input.innerHTML = back;
+   outputCalculo = outputCalculo.slice(0, -1);
+   
+return;
+  }
+
+ //funtion try and ctach error logic
+  if(text === "="){
+ 
+    try {
+        if(outputCalculo){
+          const result = new Function( `return ${outputCalculo}`,  )();
+          display__output.textContent = result;
+          outputCalculo = result.toString()
+          display__input.textContent = outputCalculo;
+
+        };
+             
+    } catch (error) {
+      display__input.innerHTML = "";
+      outputCalculo = "";
+      display__output .innerHTML = "ERROR"
+      console.log("ERROR DE CALCULO");
+            
+      
+    }
+    return;
+  }
+  display__input.textContent += text;
+  outputCalculo += text;
+  display__output.textContent = "";
+   
+} 
+
+//end try catch
+
+
+//logic for dark mode
+dMode.addEventListener("click", function () {
+  divBox.classList.toggle("mDark");
+});
+
+
